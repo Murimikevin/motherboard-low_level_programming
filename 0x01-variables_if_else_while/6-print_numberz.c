b1c3b51020aac43906e1db094fbf0d4b8b0e8a91
Murimikevin <kamaukevin069@gmail.com>
2024-03-23 13:57:04 +0300

    Made them executable

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <time.h>
+int main(void)
+{
+	/*Save the number x */
+	int x = '0';
+	/* Use the for loop  */
+	for (x = '0'; x <= '9'; x++)
+		putchar(x);
+	putchar("\n");
+	return (0);
+}
+
