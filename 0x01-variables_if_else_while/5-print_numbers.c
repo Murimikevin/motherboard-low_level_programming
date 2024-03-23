1e120d2455512eea5ca01fb8609cd355da540b77
Murimikevin <kamaukevin069@gmail.com>
2024-03-23 13:39:49 +0300

    Printing numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	/*Save the number x */
+	int x = 1;
+	/* Use the for loop  */
+	for (x = 0; x < 10; x++)
+		printf("%d", x);
+	printf("\n");
+	return (0);
+}
+
