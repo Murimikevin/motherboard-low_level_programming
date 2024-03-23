23d5c11dbf48b6926d305ec83060689c5891a831
Murimikevin <kamaukevin069@gmail.com>
2024-03-23 14:04:13 +0300

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <time.h>
+int main(void)
+{
+	/*Save the number x */
+	int x = 'z';
+	/* Use the for loop  */
+	for (x = 'z'; x >= 'a'; x--)
+		putchar(x);
+	putchar("\n");
+	return (0);
+}
